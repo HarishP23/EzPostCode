c029c8373158a91c465263b17045b19f462fade4
Harish Pandey <111580531+HarishP23@users.noreply.github.com>
2024-03-22 19:03:58 +0530

    p1

---
+++ src/app/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4">Welcome to the Home Page</h1>
+      <p className="text-lg">
+        This is a basic layout for the Home page. You can add more content and
+        components here.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
