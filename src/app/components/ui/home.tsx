ebf2f635bcb0b52fc9d923dc23e6fd2bea53e33b
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-03-22 19:56:41 +0530

    Testing

---
+++ src/app/components/ui/home.tsx
@@ -0,0 +1,14 @@
+// pages/home.tsx
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { Boxes } from "./components/ui/background-boxes";
 import { TypewriterEffectSmooth } from "./components/ui/typewriter-effect";
 import Link from "next/link";
 
-
-
 const words = [
   {
     text: "Build",
@@ -38,7 +36,7 @@ export default function Home() {
         </p>
         <TypewriterEffectSmooth words={words} />
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 justify-center">
-          <Link href="/Home.jsx" passHref>
+          <Link href="/home" passHref>
             <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
               Start now
             </button>
